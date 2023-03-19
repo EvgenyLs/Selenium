cd2933966b244d4191c8bbdef7376280d456c214
Evgeny Listov <evgenylistov@gmail.com>
2023-03-19 21:18:05 +0300

    add test_login_as_admin

---
+++ test_login_as_admin.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/login.php")
+    driver.find_element(By.NAME, "username").send_keys("admin")
+    driver.find_element(By.NAME, "password").send_keys("admin")
+    driver.find_element(By.NAME, "login").click()
+    time.sleep(3)
+
+
+
